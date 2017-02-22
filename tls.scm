18528bc9be10ab8d470bc6f72fe6e34ac94b78bc
Ben Trevett <bentrevett@users.noreply.github.com>
2017-02-22 07:39:00 +0000

    first 3 chapters of TLS in the tls.scm

--- tls.scm
+++ tls.scm
@@ -7,3 +7,19 @@
 (define rember (lambda (a lat) (cond ((null? lat) (quote ())) ((eq? (car lat) a) (cdr lat)) (else (cons (car lat) (rember a (cdr lat)))))))
 
 (define firsts (lambda (l) (cond ((null? l) '()) (else (cons (car (car l)) (firsts (cdr l)))))))
+
+(define insertR (lambda (new old lat) (cond ((null? lat) '()) (else (cond ((eq? (car lat) old) (cons old (cons new (cdr lat)))) (else (cons (car lat) (insertR new old (cdr lat)))))))))
+
+(define insertL (lambda (new old lat) (cond ((null? lat) '()) (else (cond ((eq? (car lat) old) (cons new (cons old (cdr lat)))) (else (cons (car lat) (insertL new old (cdr lat)))))))))
+
+(define subst (lambda (new old lat) (cond ((null? lat) '()) (else (cond ((eq? (car lat) old) (cons new (cdr lat))) (else (cons (car lat) (subst new old (cdr lat)))))))))
+
+(define subst2 (lambda (new old1 old2 lat) (cond ((null? lat) '()) (else (cond ((eq? (car lat) old1) (cons new (cdr lat))) ((eq? (car lat) old2) (cons new (cdr lat))) (else (cons (car lat) (subst2 new old1 old2 (cdr lat)))))))))
+
+(define multirember (lambda (a lat) (cond ((null? lat) '()) (else (cond ((eq? (car lat) a) (multirember a (cdr lat))) (else (cons (car lat) (multirember a (cdr lat)))))))))
+
+(define multiinsertrR (lambda (new old lat) (cond ((null? lat) '()) (else (cond ((eq? (car lat) old) (cons (car lat) (cons new (multiinsertR new old (cdr lat))))) (else (cons (car lat) (multiinsertR new old (cdr lat)))))))))
+
+(define multiinsertL (lambda (new old lat) (cond ((null? lat) '()) (else (cond ((eq? (car lat) old) (cons new (cons old (multiinsertL new old (cdr lat))))) (else (cons (car lat) (multiinsertL new old (cdr lat)))))))))
+
+(define multisubs (lambda (new old lat) (cond ((null? lat) '()) (else (cond ((eq? (car lat) old) (cons new (multisubst new old (cdr lat)))) (else (cons (car lat) (multisubst new old (cdr lat)))))))))
